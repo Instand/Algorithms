7dc41dfe71c61198e10b8bc5872a350a3586cffa
Arew <noobterr@yandex.ru>
2019-04-17 13:53:02 +0300

    Add merge sort realization

--- sort/mergesort.hpp
+++ sort/mergesort.hpp
@@ -2,36 +2,56 @@
 #define MERGESORT_HPP
 
 #include <vector>
-#include <deque>
 #include <iterator>
 #include <utility>
+
+// worst-case O(n log m)
+// avarage-case O(n log n)
+// best-case O(n log n)
+
+// memory space O(n)
+
+// stable
 
 namespace cs {
     template <typename Iter>
     void merge(Iter begin, Iter medium, Iter end) {
-        std::deque<typename Iter::value_type> temp;
+      using Value = typename Iter::value_type;
 
-        auto iter = medium;
-        for (auto size = std::distance(begin, iter); size > 0; --size, --iter) {
-            temp.push_front(*iter);
-        }
+      std::vector<Value> first(begin, medium);
+      std::vector<Value> second(medium, end);
 
-        for (auto i = std::next(medium); i != end; ++i) {
-            temp.push_back(*i);
-        }
+      auto firstBegin = first.begin();
+      auto secondBegin = second.begin();
+
+      for (auto iter = begin; iter != end; ++iter) {
+          if (firstBegin == first.end()) {
+              std::iter_swap(iter, secondBegin++);
+          }
+          else if (secondBegin == second.end()) {
+              std::iter_swap(iter, firstBegin++);
+          }
+          else if ((*firstBegin) < (*secondBegin)) {
+              std::iter_swap(firstBegin++, iter);
+          }
+          else {
+              std::iter_swap(secondBegin++, iter);
+          }
+      }
     }
 
     template <typename Iterator>
     void mergeSort(Iterator begin, Iterator end) {
-        if (begin == end) {
+        auto lenght = std::distance(begin, end);
+
+        if (lenght < 2) {
             return;
         }
 
-        auto lenght = std::distance(begin, end);
         Iterator medium = std::next(begin, lenght/2);
 
         mergeSort(begin, medium);
-        mergeSort(std::next(medium), end);
+        mergeSort(medium, end);
 
         merge(begin, medium, end);
     }
